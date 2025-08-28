e786a76b1ee6a35242eb19a0aab70d3ecac5f11a
wewz12332 <bennylabajo03@gmail.com>
2025-08-28 08:08:55 +0800

    Add files via upload

--- practice to js/scripts.js
+++ practice to js/scripts.js
@@ -1,26 +1,29 @@
-const display = document.getElementById("display");
-const mySubmit = document.getElementById("mySubmit");
+const myCheckbox = document.getElementById("myCheckbox");
+const visaBtn = document.getElementById("visaBtn");
+const masterCardBtn = document.getElementById("masterCardBtn");
+const paypalBtn = document.getElementById("paypalBtn");
+const submitBtn = document.getElementById("submitBtn");
 const result = document.getElementById("result");
+const paymentResult = document.getElementById("paymentResult");
 
-let bmi = 0;
+submitBtn.onclick = function(){
 
-
-mySubmit.onclick = function(){
-    bmi = display.value;
-    bmi = Number(bmi);
-
-    if (bmi < 18.5){
-        result.textContent = "You are Under Weight!";
-    } else if (bmi >= 18.5 && bmi <= 24.9){
-        result.textContent = "You are Normal Weight!";
-    } else if (bmi >= 25.0 && bmi <= 29.9){
-        result.textContent = "You are Over Weight!";
-    } else if (bmi >= 30.0 && bmi <= 34.9){
-        result.textContent = "You are Obese!";
-    } else if (bmi >= 35.0){
-        result.textContent = "You are Extremely Obese!";
+    if(myCheckbox.checked){
+        result.textContent = `You are subscribed!`;
+    } 
+    else {
+        result.textContent = `You are NOT Subscribe!`;
+    }
+    if (visaBtn.checked){
+        paymentResult.textContent = `You are paying with Visa!`;
+    
+    } else if (masterCardBtn.checked){
+        paymentResult.textContent = `You are paying with Master Card`;
+    
+    } else if (paypalBtn.checked){
+        paymentResult.textContent = `You are paying with Paypal`;
+    
     } else {
-        bmi = NaN;
-        result.textContent = "Numbers Only";
+        paymentResult.textContent = `You must select a payment type!`;
     }
 }
